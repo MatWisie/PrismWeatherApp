adff2c763374771d08e5f88dc32f6f017ce92889
MatWisie <Mateuszwisiecki@gmail.com>
2023-08-24 22:58:20 +0200

    Added PrismWeatherApp.Search module

---
+++ PrismWeatherApp.Search/SearchModule.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using PrismWeatherApp.Search.Views;
+
+namespace PrismWeatherApp.Search
+{
+    public class SearchModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
