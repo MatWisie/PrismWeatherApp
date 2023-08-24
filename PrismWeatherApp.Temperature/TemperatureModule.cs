50a72c371ebb1f870be675e8abe9a1f4e5521f5b
MatWisie <Mateuszwisiecki@gmail.com>
2023-08-24 22:58:48 +0200

    Added PrismWeatherApp.Temperature module

---
+++ PrismWeatherApp.Temperature/TemperatureModule.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using PrismWeatherApp.Temperature.Views;
+
+namespace PrismWeatherApp.Temperature
+{
+    public class TemperatureModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
