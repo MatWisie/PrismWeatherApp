5bd2d1b1c11d8dbdc41421917a932f7a1ee9258a
MatWisie <Mateuszwisiecki@gmail.com>
2023-08-27 21:36:00 +0200

    Registered Menu module

--- PrismWeatherApp.Menu/MenuModule.cs
+++ PrismWeatherApp.Menu/MenuModule.cs
@@ -1,15 +1,21 @@
 ﻿using Prism.Ioc;
 using Prism.Modularity;
 using Prism.Regions;
+using PrismWeatherApp.Core;
 using PrismWeatherApp.Menu.Views;
 
 namespace PrismWeatherApp.Menu
 {
     public class MenuModule : IModule
     {
+        private readonly IRegionManager _regionManager;
+        public MenuModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
         public void OnInitialized(IContainerProvider containerProvider)
         {
-
+            _regionManager.RegisterViewWithRegion(RegionNames.MenuRegion, typeof(ViewA));
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
