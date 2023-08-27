bf1b6879d9287b7e26beb53b4df78359a0ffda2d
MatWisie <Mateuszwisiecki@gmail.com>
2023-08-27 21:25:17 +0200

    Added City model

---
+++ PrismWeatherApp.Core/Models/City.cs
@@ -0,0 +1,20 @@
+﻿namespace PrismWeatherApp.Core.Models
+{
+    public class City
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public string feature_code { get; set; }
+        public string country_code { get; set; }
+        public string country { get; set; }
+        public string displayPath
+        {
+            get
+            {
+                return name + ", " + country;
+            }
+        }
+    }
+}
