0eaefb49079f96d57604d5e7d0aea3083cf4e367
MatWisie <Mateuszwisiecki@gmail.com>
2023-08-27 21:27:44 +0200

    Added RegionNames and implemented them in MainWindow and TemperatureModule view

---
+++ PrismWeatherApp.Core/RegionNames.cs
@@ -0,0 +1,9 @@
+﻿namespace PrismWeatherApp.Core
+{
+    public static class RegionNames
+    {
+        public static string ContentRegion = "ContentRegion";
+        public static string MenuRegion = "MenuRegion";
+        public static string SearchRegion = "SearchRegion";
+    }
+}
