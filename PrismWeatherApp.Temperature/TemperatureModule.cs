72dfdf44bb6d323508f2b61e8dccafd6607515c5
MatWisie <Mateuszwisiecki@gmail.com>
2023-08-27 21:29:53 +0200

    Registered TemperatureModule

--- PrismWeatherApp.Temperature/TemperatureModule.cs
+++ PrismWeatherApp.Temperature/TemperatureModule.cs
@@ -1,15 +1,22 @@
 ﻿using Prism.Ioc;
 using Prism.Modularity;
 using Prism.Regions;
+using PrismWeatherApp.Core;
 using PrismWeatherApp.Temperature.Views;
 
 namespace PrismWeatherApp.Temperature
 {
     public class TemperatureModule : IModule
     {
+        private readonly IRegionManager _regionManager;
+        public TemperatureModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
         public void OnInitialized(IContainerProvider containerProvider)
         {
-
+            _regionManager.RegisterViewWithRegion(RegionNames.ContentRegion, typeof(ViewA));
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
