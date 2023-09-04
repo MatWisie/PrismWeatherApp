ed422c45725c4cd1ce838e9872c0952261be962f
MatWisie <Mateuszwisiecki@gmail.com>
2023-09-04 12:58:40 +0200

    Added Temperature model

---
+++ PrismWeatherApp.Core/Models/Temperature.cs
@@ -0,0 +1,9 @@
+﻿namespace PrismWeatherApp.Core.Models
+{
+    public class Temperature
+    {
+        public double Latitiude { get; set; }
+        public double Longitiude { get; set; }
+        public TemperatureHourly Hourly { get; set; }
+    }
+}
