188b36790c495faf9d372bcbf45b4c34417412ca
MatWisie <Mateuszwisiecki@gmail.com>
2023-09-04 12:59:02 +0200

    Added TemperatureStatic model

---
+++ PrismWeatherApp.Core/TemperatureStatic.cs
@@ -0,0 +1,12 @@
+﻿using PrismWeatherApp.Core.Models;
+
+namespace PrismWeatherApp.Core
+{
+    public static class TemperatureStatic
+    {
+        public static string CityName { get; set; }
+        public static double Latitiude { get; set; }
+        public static double Longitiude { get; set; }
+        public static TemperatureHourly Hourly { get; set; }
+    }
+}
