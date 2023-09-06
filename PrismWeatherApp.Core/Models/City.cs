70817666b00bb906bd5d1e1ab7143295cb67bb5d
MatWisie <Mateuszwisiecki@gmail.com>
2023-09-06 13:21:55 +0200

    Fixed City

--- PrismWeatherApp.Core/Models/City.cs
+++ PrismWeatherApp.Core/Models/City.cs
@@ -4,8 +4,8 @@
     {
         public int id { get; set; }
         public string name { get; set; }
-        public double latitude { get; set; }
-        public double longitude { get; set; }
+        public float latitude { get; set; }
+        public float longitude { get; set; }
         public string feature_code { get; set; }
         public string country_code { get; set; }
         public string country { get; set; }
