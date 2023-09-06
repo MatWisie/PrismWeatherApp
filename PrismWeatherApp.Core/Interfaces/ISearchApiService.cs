641bd7339c38318f4b4b17bba582d58ea5f5b7da
MatWisie <Mateuszwisiecki@gmail.com>
2023-09-06 13:19:29 +0200

    Moved ISearchApiService to different file

---
+++ PrismWeatherApp.Core/Interfaces/ISearchApiService.cs
@@ -0,0 +1,10 @@
+﻿using PrismWeatherApp.Core.Models;
+
+namespace PrismWeatherApp.Core.Interfaces
+{
+    public interface ISearchApiService
+    {
+        Task<CitiesResult> GetCities(string city);
+        Task<Temperature> GetTemperature(float latitiude, float longitiude);
+    }
+}
