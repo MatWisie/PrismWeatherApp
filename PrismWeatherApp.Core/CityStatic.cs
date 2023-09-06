da30f98fdebfa7270d35a6b0167d8f35694c6ee2
MatWisie <Mateuszwisiecki@gmail.com>
2023-09-06 13:22:08 +0200

    Created CityStatic

---
+++ PrismWeatherApp.Core/CityStatic.cs
@@ -0,0 +1,13 @@
+﻿namespace PrismWeatherApp.Core
+{
+    public static class CityStatic
+    {
+        public static int id { get; set; }
+        public static string name { get; set; }
+        public static float latitude { get; set; }
+        public static float longitude { get; set; }
+        public static string feature_code { get; set; }
+        public static string country_code { get; set; }
+        public static string country { get; set; }
+    }
+}
