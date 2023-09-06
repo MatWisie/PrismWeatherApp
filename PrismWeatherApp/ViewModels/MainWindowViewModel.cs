2a9b0b196cab76b5de9769f64d92aa2b997c19d7
MatWisie <Mateuszwisiecki@gmail.com>
2023-09-06 13:25:52 +0200

    Implemented loading from json to static CityStatic model in MainWindowViewModel

--- PrismWeatherApp/ViewModels/MainWindowViewModel.cs
+++ PrismWeatherApp/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,33 @@
 ﻿using Prism.Mvvm;
+using PrismWeatherApp.Core;
+using PrismWeatherApp.Core.Interfaces;
+using PrismWeatherApp.Core.Models;
 
 namespace PrismWeatherApp.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
+        public ISearchApiService _searchApiService { get; }
+        public MainWindowViewModel(ISearchApiService searchApiService)
+        {
+            _searchApiService = searchApiService;
+
+            //TODO: find cleaner way to load city
+            City currentCity = JsonServices.LoadJson<City>("currentLoc.json");
+            CityStatic.country = currentCity.country;
+            CityStatic.name = currentCity.name;
+            CityStatic.latitude = currentCity.latitude;
+            CityStatic.longitude = currentCity.longitude;
+            CityStatic.feature_code = currentCity.feature_code;
+            CityStatic.country_code = currentCity.country_code;
+            CityStatic.id = currentCity.id;
+
+            var tmpTemperature = _searchApiService.GetTemperature(CityStatic.latitude, CityStatic.longitude).Result;
+            TemperatureStatic.CityName = CityStatic.name;
+            TemperatureStatic.Latitiude = tmpTemperature.latitude;
+            TemperatureStatic.Longitiude = tmpTemperature.longitude;
+            TemperatureStatic.Hourly = tmpTemperature.hourly;
+        }
+
     }
 }
