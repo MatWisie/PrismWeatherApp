1a5ec0a8e8974e8ecdaf71c597c98441bf6456a0
MatWisie <Mateuszwisiecki@gmail.com>
2023-09-06 13:24:17 +0200

    Created SaveJson method in JsonServices

--- PrismWeatherApp.Core/JsonServices.cs
+++ PrismWeatherApp.Core/JsonServices.cs
@@ -5,5 +5,23 @@ namespace PrismWeatherApp.Core
 {
     public static class JsonServices
     {
+        public static bool SaveJson(object obj)
+        {
+            try
+            {
+                if (obj == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                string json = JsonConvert.SerializeObject(obj);
+                System.IO.File.WriteAllText("CurrentLoc.json", json);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex.Message);
+                return false;
+            }
+        }
     }
 }
