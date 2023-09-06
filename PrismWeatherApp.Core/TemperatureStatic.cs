c902b90d7493dd743e9fe2ca492a51db34d350e2
MatWisie <Mateuszwisiecki@gmail.com>
2023-09-06 13:21:25 +0200

    Fixed TemperatureStatic

--- PrismWeatherApp.Core/TemperatureStatic.cs
+++ PrismWeatherApp.Core/TemperatureStatic.cs
@@ -5,8 +5,8 @@ namespace PrismWeatherApp.Core
     public static class TemperatureStatic
     {
         public static string CityName { get; set; }
-        public static double Latitiude { get; set; }
-        public static double Longitiude { get; set; }
+        public static float Latitiude { get; set; }
+        public static float Longitiude { get; set; }
         public static TemperatureHourly Hourly { get; set; }
     }
 }
